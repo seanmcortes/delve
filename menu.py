e41db2d4352dd175d379c44c47b77a7b3dfcecdd
jaydee3 <jaydee3@users.noreply.github.com>
2019-04-21 16:58:38 -0700

    added Load Game menu and save files

--- menu.py
+++ menu.py
@@ -7,7 +7,9 @@ from sprites import *
 import scenes
 from scenes import *
 from helper import *
-#
+from os import listdir #for file handling
+from os.path import isfile, join #for file handling
+
 #Source: https://stackoverflow.com/questions/28005641/how-to-add-a-background-image-into-pygame
 class Background(pygame.sprite.Sprite):
 	def __init__(self, image_file, location):
@@ -23,12 +25,15 @@ class TextObject():
 
 		if align == "center":
 			self.rect.center = (x, y)
+		else:
+			self.rect.left = x
+			self.rect.top = y
 
 	def render(self,screen):
 		 screen.blit(self.surface, self.rect)
 
 class MenuButton():
-	def __init__(self, game, msg, location, action=None):
+	def __init__(self, game, msg, location, action=None, optional_argument=None):
 		pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
 		self.msg = msg
 		self.img = self.img_holder()
@@ -45,6 +50,7 @@ class MenuButton():
 		self.textRect.center = ( (self.x+(self.w/2)), (self.y+(self.h/2)) )
 		self.NORMAL = True
 		self.CLICKED = False
+		self.optional_argument = optional_argument
 
 	class img_holder():
 		def __init__(self):
@@ -117,19 +123,17 @@ class MainMenuScene():
 		self.game.go_to(scenes.Level1Scene())
 
 	def loadgame(self):
-		self.game.go_to(GameOverScene(self.game))
+		self.game.go_to(LoadGameScene(self.game))
 
 	def mainmenu(self):
 		self.game.go_to(MainMenuScene(self.game))
 
 	def render(self, screen):
 		self.dt = self.game.clock.tick(FPS) / 1000
-		self.game.screen.fill(BLACK)
+		#self.game.screen.fill(BLACK)
 		self.game.screen.blit(self.background.image, self.background.rect)
 		for t in self.textObjects:
 			t.render(self.game.screen)
-		#self.game.screen.blit(self.logoSurf, self.logoRect)
-		#text_to_screen(self.game.screen, "DELVE", 'image/CuteFont-Regular.ttf', 100, WHITE, WIDTH / 2, HEIGHT / 4)
 
 		for p in self.all_buttons:
 			p.draw()
@@ -142,7 +146,10 @@ class MainMenuScene():
 				p.draw()
 				pygame.display.flip()
 				pygame.time.delay(500)
-				p.action()
+				if p.optional_argument == None:
+					p.action()
+				else: #this is used to load a level
+					p.action(p.optional_argument)
 
 	"""
 	Event listener for main menu.
@@ -176,7 +183,58 @@ class GameOverScene(MainMenuScene):
 		self.text_logo = TextObject("GAME OVER", 'image/CuteFont-Regular.ttf', 100, WHITE, WIDTH / 2, HEIGHT / 4)
 		self.text_message = TextObject("Would you like to continue?", 'image/CuteFont-Regular.ttf', 50, WHITE, WIDTH / 2, (HEIGHT / 3)+50)
 		self.textObjects = [self.text_logo, self.text_message]
-		self.all_buttons = []
 		self.button1 = MenuButton(self.game, "Yes", [270,350], self.playgame)
 		self.button2 = MenuButton(self.game, "No", [270,425], self.mainmenu)
 		self.all_buttons = [self.button1, self.button2]
+
+class LoadGameScene(MainMenuScene):
+	def __init__(self, game):
+		#super().__init__(game)
+		self.game = game
+		self.background = Background('image/menuback.jpg', [0,0])
+		self.text_logo = TextObject("Load Game", 'image/CuteFont-Regular.ttf', 100, WHITE, WIDTH / 2, HEIGHT / 4)
+		self.textObjects = [self.text_logo]
+		self.button1 = MenuButton(self.game, "Back", [270,450], self.mainmenu)
+		self.all_buttons = [self.button1]
+		save_path = 'save'
+		#Source: https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+		#gets only files not any directories
+		y = 250
+		count = 0
+		files = [f for f in listdir(save_path) if isfile(join(save_path, f))]
+		for o in files:
+			f = open(join(save_path, o), "r")
+			if f.mode == 'r':
+				try:
+					number = int(f.readline().strip())
+				except:
+					number = "Not an integer"
+				if isinstance(number, int) and count < 3: #to do: make sure number is an actual level
+					date = f.readline().strip()
+					save_text = "Level: " + str(number) + " Time: " + date
+					self.textObjects.append(TextObject(save_text, 'image/CuteFont-Regular.ttf', 40, WHITE, 210, y+14, "left"))
+					self.all_buttons.append(MenuButton(self.game, "Load", [100, y], self.loadlevel, number))
+					y += 60
+					count += 1
+
+	def loadlevel(self, level):
+		#Source: https://jaxenter.com/implement-switch-case-statement-python-138315.html
+		switcher = {
+	        1: scenes.Level1Scene,
+	        2: GameOverScene,
+	        3: GameOverScene,
+	        4: GameOverScene,
+	        5: GameOverScene,
+	        6: GameOverScene,
+	        7: GameOverScene,
+	        8: GameOverScene,
+	        9: GameOverScene,
+	        10: GameOverScene,
+		}
+	    # Get the function from switcher dictionary
+		func = switcher.get(level, lambda: "Invalid level")
+		# Execute the function
+		if level > 1: #this is just here until we implement the other levels, becuase GameOverScene requires the game be passed to it
+			self.game.go_to(func(self.game))
+		else:
+			self.game.go_to(func())
