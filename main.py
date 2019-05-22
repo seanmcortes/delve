7d70302c334b1def429f2188918aba37bc3125fb
Sean Cortes <cortess@oregonstate.edu>
2019-05-22 13:51:30 -0700

    add import for level 8

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame.freetype
 from settings import *
 from sprites import *
 from scenes import TutorialMovement, TutorialBlock, TutorialEnemy, TutorialIce, \
-    BlockUnitTest, Level5, Level6, Level7, jasonlevel
+    BlockUnitTest, Level5, Level6, Level8, jasonlevel
 from menu import *
 
 """
